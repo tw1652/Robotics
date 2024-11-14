f2057a9d954626af4d9168d766f6e9efc1cab888
tw1652 <117903509+tw1652@users.noreply.github.com>
2024-11-14 15:18:15 +0000

    14/11/24 UltrasonicSensor.ino
    Fully working ultrasonic sensor

--- UltrasonicSensor.ino
+++ UltrasonicSensor.ino
@@ -1,7 +1,6 @@
 #include <mbed.h>
 
 using namespace mbed;
-//Serial.begin(9600);
 
 DigitalInOut trig(P1_14);//puts it on sensor 2 position on the board
 DigitalInOut echo(P1_14);
@@ -12,8 +11,10 @@ int correction = 0;
 void setup() {
 }
 
-//I've put setup stuff here
-//This is for the correction, might not be needed methinks!
+//I've put setup stuff here, this is for the correction, might not be needed methinks!
+//This  will give how much delay there is in just turning on and off the timer
+//if it turns out that sensor is innaccurate, put this in
+//if not leave it out, more efficient code without it
 /*timer.reset();
 timer.start();
 timer.stop();
@@ -21,8 +22,7 @@ correction = timer.read_us();
 Serial.print("Approx software overhead timer delay is %d uS\n\r",correction);*/
 
 void loop() {
-  // trigger the ultrasonic sensor to send a pulse
-  // this will send a pulse for 10uS
+  // trigger the ultrasonic sensor to send a pulse for 10uS
   trig.output();// sets pin to output
   trig = 1;
   timer.reset();
@@ -37,12 +37,10 @@ void loop() {
   timer.start();
   while (echo==1) {};
   timer.stop();
-  //divide by 58 as that's what it says on datasheet for cm
-  distance = (timer.read_us()/58.0);
-  Serial.print("Distance in cm: ");
-  Serial.print(distance, DEC);//print distance in decimal (no it doesn't)
+  distance = (timer.read_us()/58.0);  //divide by 58 as per datasheet for conversion to cm
   Serial.println(); // print blank line
-  wait_us(60000); //wait 60ms so any echoes return before sending another pulse
-  //as per datasheet reccomendation
-
+  Serial.print("Distance in cm: ");
+  Serial.print(distance, DEC);//print distance in decimal
+  Serial.println(); 
+  delay(100); //wait so any echoes return before sending another pulse as per datasheet reccomendation
 }
