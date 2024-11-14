07fde33be6c465f5f9291340aab4a7b5c495522b
tw1652 <117903509+tw1652@users.noreply.github.com>
2024-11-14 14:44:47 +0000

    Update Lab5.ino
    Add other IR sensor capability

--- Lab5.ino
+++ Lab5.ino
@@ -8,6 +8,7 @@ void setup() {
 void loop() {
   I2C i2c(P0_31, P0_2);              // initializes i2c, uses P0_31 as SDA (for data) and P0_2 as SCL (clock)
   const char mux_cmd = 0x01;         // defines mux_cmd as command to select a channel on the I2C mux. Addresss in hex as 1, in binary it is 0001
+  //const char mux_cmd = 0x02;  //this selects the other IR sensor when uncommented
   const char mux_addr = 0xEE;        // select bus 0 on mux chip
   i2c.write(mux_addr, &mux_cmd, 1);  // writes the mux_cmd to the addresss mux_addr
   //the sensor is now on bus 0, address 0x80
