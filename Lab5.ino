67bbdddba5b9da44cb2239aa5a8b20973105f5f0
tw1652 <117903509+tw1652@users.noreply.github.com>
2024-11-14 13:23:07 +0000

    14/11/24 Lab5.ino
    Managed to get reading from IR sensor, great success

--- Lab5.ino
+++ Lab5.ino
@@ -5,44 +5,24 @@ using namespace mbed;
 void setup() {
 }
 
-
 void loop() {
-  I2C i2c(P0_31, P0_2); // initializes i2c, uses P0_31 as SDA (for data) and P0_2 as SCL (clock)
-  const char mux_cmd = 0x01; // defines mux_cmd as command to select a channel on the I2C mux. Addresss in hex as 1, in binary it is 0001
-  const char mux_addr = 0xEE; // select bus 0 on mux chip
-  i2c.write(mux_addr, &mux_cmd, 1); // writes the mux_cmd to the addresss mux_addr  
+  I2C i2c(P0_31, P0_2);              // initializes i2c, uses P0_31 as SDA (for data) and P0_2 as SCL (clock)
+  const char mux_cmd = 0x01;         // defines mux_cmd as command to select a channel on the I2C mux. Addresss in hex as 1, in binary it is 0001
+  const char mux_addr = 0xEE;        // select bus 0 on mux chip
+  i2c.write(mux_addr, &mux_cmd, 1);  // writes the mux_cmd to the addresss mux_addr
   //the sensor is now on bus 0, address 0x80
-  
-  //Read shift bit
-  //char n;
-  //i2c.read(0x35, &n, 1);
-  int n = 2.04;
 
-  // ir sensor has a 7 bit slave address so must be shifted by 1 bit before passing
-  // how does that apply to this??
-
-  char distances[3];//array distances with 3 values
-  distances[0] = 0x5E;//high distance
-  distances[1] = 0x5F;//low distance
+  char distances[3];    //array distances with 3 values
+  distances[0] = 0x5E;  //high distance
+  distances[1] = 0x5F;  //low distance
   distances[2] = 0x00;
-  i2c.write(0x80, distances, 1);// write register addresss to read from
-  wait_us(500000); // wait for sensor response
-  i2c.read(0x80, distances, 3);  //Read distance high 
-
-  float dist = ((distances[0]*16)+(distances[1]))/(16/(2^n));
-
-  /*Serial.println(); // print blank line
-  Serial.println("high distance");
+  i2c.write(0x80, distances, 1);  // write register addresss to read from
+  wait_us(500000);                // wait for sensor response
+  i2c.read(0x80, distances, 3);   //Read distance high
+  
   Serial.println();
-  Serial.print(distances[0], DEC);
-  Serial.println();
-  Serial.println("low distance");
-  Serial.println(); 
-  Serial.print(distances[1], DEC);
-  //Serial.println();
-  //Serial.println("shift bit");
-  //Serial.println();
-  //Serial.println(n);
-  //Serial.println();*/
   Serial.print("Distance in cm: ");
-  Serial.println(dist, 3);//Print distance
+  Serial.print((distances[0]*16.0+distances[1])/16.0/4);
+  Serial.println();
+  delay(100);
+}
