02e3c2184ea2eb13e71c0d82cf681f349a6f1737
tw1652 <117903509+tw1652@users.noreply.github.com>
2024-11-15 12:39:05 +0000

    15/11/24 Lab6_Motors.ino
    Motors all working with a brake method too

--- Lab6_Motors.ino
+++ Lab6_Motors.ino
@@ -1,7 +1,6 @@
 #include <mbed.h>
 
 using namespace::mbed;
-//Serial.begin(9600);
 
 PwmOut MotorAPWM(P0_27);
 DigitalOut MotorADir(P0_4);
@@ -9,50 +8,69 @@ PwmOut MotorBPWM(P1_2);
 DigitalOut MotorBDir(P0_5);
 
 void setup(){
+  Serial.begin(9600);
 }
 
 class Motor {
   public:
     int moveMotor(int motor, int direction, float speed);
+    int brakeMotor(int motor);
 };
 
 int Motor::moveMotor(int motor, int direction, float speed) {
 
-  if (motor = 0) {
-    if (direction = 0) {
+  if (motor == 0) {
       Serial.println("Motor A selected");
       MotorAPWM.period(0.0001f); // Sets frequency to 100kHz
-      //set direction forward
+      MotorADir.write(direction);//set direction forward
       MotorAPWM.write(speed); 
-    }
-    if (direction  = 1) {
-      Serial.println("Motor A selected");
-      MotorAPWM.period(0.0001f); // Sets frequency to 100kHz
-      //set direction backward
-      MotorAPWM.write(speed);   
-    }
-
   }
-  if (motor = 1) {
-    if (direction = 0) {
+  else if (motor == 1) {
       Serial.println("Motor B selected");
       MotorBPWM.period(0.0001f); // Sets frequency to 100kHz
-      //set direction forward
+      MotorBDir.write(direction);//set direction
       MotorBPWM.write(speed);
-    } 
-    if (direction = 1) {
-      Serial.println("Motor B selected");
-      MotorBPWM.period(0.0001f); // Sets frequency to 100kHz
-      //set direction backward
-      MotorBPWM.write(speed);
-    }
   }
   else {
     Serial.println("re-enter motor number");
   }
 }
 
+int Motor::brakeMotor(int motor){
+
+  if (motor == 0) {
+    Serial.println("Motor A braking");
+    MotorAPWM.write(0);//pwm input set low
+    //set high z as per datasheet
+    MotorADir.write(0);
+    MotorAPWM.write(0);
+  }
+  
+  if (motor ==1 ) {
+    Serial.println("Motor B braking");
+    MotorBPWM.write(1);//pwm input set low
+    //set high z
+    MotorBDir.write(0);
+    MotorBPWM.write(0);
+  }
+}
+
 void loop() {
   Motor motor;
   motor.moveMotor(0, 0, 0.5f);
+  delay(1000);
+  motor.brakeMotor(0);
+  delay(1000);
+  motor.moveMotor(0, 1, 0.5f);
+  delay(1000);
+  motor.brakeMotor(0);
+  delay(1000);
+  motor.moveMotor(1, 0, 0.75f);
+  delay(1000);
+  motor.brakeMotor(1);
+  delay(1000);
+  motor.moveMotor(1, 1, 0.75f);
+  delay(1000);
+  motor.brakeMotor(1);
+  delay(1000);
 }
